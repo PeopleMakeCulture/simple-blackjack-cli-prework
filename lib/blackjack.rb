5d9a3245301483ad4c4e033596517fa0df401118
Jing Cao <jingcao.yale@gmail.com>
2019-01-26 02:52:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,9 +58,12 @@ end
 #####################################################
 
 def runner
-  # calls on the #welcome method,
-  # then on the #initial_round method,
-  # then calls #hit? and #display_card_total methods
-  # -until- the card sum is greater than 21,
-  # then calls on the #end_game method
+  welcome
+  initial_round
+   and #display_card_total methods
+  until card_total > 21
+    hit?
+  end
+  end_game
+
 end
